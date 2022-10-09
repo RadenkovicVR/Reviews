54703f9b3ff3e5df2011bd7c6408c3ce4881c40a
RadenkovicVR <radenkovic.aleksandar2000@gmail.com>
2022-10-09 12:43:41 +0200

    Desc div fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const jobList = ['WEB DEVELOPER', "BOOKMAKER", "EVENTS ORGANIZER", "STREET ENTER
 const descriptionList = 
 ['Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni maxime rerum voluptatem at enim expedita veniam praesentium ut fugiat, sint doloribus consequatur neque ad voluptate a voluptatum voluptas itaque dicta?',
 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum vestibulum ullamcorper. Donec pulvinar tortor nec metus tristique, vitae congue diam sagittis. Vivamus id nulla eleifend, gravida neque at, iaculis nibh.',
-'Morbi quis ante diam. Donec massa magna, rhoncus ut luctus sit amet, condimentum quis neque. Vivamus a hendrerit metus, ac tristique est. Nullam non ultricies mauris. Vivamus id nulla eleifend',
+'Morbi quis ante diam. Donec massa magna, rhoncus ut luctus sit amet, condimentum quis neque. Vivamus a hendrerit metus, ac tristique est. Nullam non ultricies mauris. Vivamus id nulla eleifend, gravida neque at, iaculis nibh.',
 'Vivamus egestas convallis vestibulum. Aliquam leo sapien, commodo lacinia magna et, lacinia elementum mauris. Fusce risus mauris, ullamcorper a interdum at, vehicula sit amet nulla. Suspendisse eget purus sit amet ante varius malesuada sed sit amet lectus.'];
 
 
